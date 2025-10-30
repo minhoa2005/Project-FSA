465d0c1b70842d4ebb8b423d44155dcc75f146b2
Tran Duc Minh <minhsoc5@gmail.com>
2025-10-30 10:50:19 +0700

    setup header, short api, deleted unnecessary

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, PATCH, UPDATE',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'Content-Type, Authorization',
+          },
+        ]
+      }
+    ]
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'http://localhost:3000/api/:path*',
+      }
+    ]
+  },
   reactCompiler: true,
+
 };
 
 export default nextConfig;
