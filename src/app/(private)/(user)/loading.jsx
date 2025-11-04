c2bc3fedda1067323de08d4cfa7d37c894fb935f
Tran Duc Minh <minhsoc5@gmail.com>
2025-11-04 16:18:48 +0700

    test loading

---
+++ src/app/(private)/(user)/loading.jsx
@@ -0,0 +1,9 @@
+import { Spinner } from "@/components/ui/spinner"
+
+export default function loading() {
+    return (
+        <div className="flex items-center h-[100vh] justify-center">
+            <Spinner size="large" className={"w-10 h-10"} />
+        </div>
+    )
+}
