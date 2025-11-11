4f6a774eac03addc56ff6e02665cadb018a18bc5
Lowgfive <baolong2000k4@gmail.com>
2025-11-11 13:54:26 +0700

    Long check

--- src/app/(private)/(user)/loading.jsx
+++ src/app/(private)/(user)/loading.jsx
@@ -4,6 +4,7 @@ export default function loading() {
     return (
         <div className="flex items-center h-[100vh] justify-center">
             <Spinner size="large" className={"w-10 h-10"} />
+            
         </div>
     )
 }
