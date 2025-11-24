34b66fa7ff7f4c415682ed6d76bb44ec29bb0466
Tran Duc Minh <minhsoc5@gmail.com>
2025-11-24 13:15:56 +0700

    update shadcn

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,21 +1,16 @@
-"use client";
+"use client"
+
 import { useTheme } from "next-themes"
 import { Toaster as Sonner } from "sonner"
-import {
-  CheckCircle2,
-  XCircle,
-  AlertTriangle,
-  Info,
-} from "lucide-react"
 
-const Toaster = ({
-  ...props
-}) => {
+type ToasterProps = React.ComponentProps<typeof Sonner>
+
+const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = "system" } = useTheme()
 
   return (
     <Sonner
-      theme={theme}
+      theme={theme as ToasterProps["theme"]}
       className="toaster group"
       toastOptions={{
         classNames: {
@@ -28,8 +23,9 @@ const Toaster = ({
             "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
         },
       }}
-      {...props} />
-  );
+      {...props}
+    />
+  )
 }
 
 export { Toaster }
