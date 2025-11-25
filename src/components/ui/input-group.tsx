cb8e48a101b0d8cec485f334aa816cdc982a6761
Tran Duc Minh <minhsoc5@gmail.com>
2025-11-25 09:07:42 +0700

    upload avatar 1

--- src/components/ui/input-group.tsx
+++ src/components/ui/input-group.tsx
@@ -1,17 +1,14 @@
 "use client"
 
 import * as React from "react"
-import { cva } from "class-variance-authority";
+import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 
-function InputGroup({
-  className,
-  ...props
-}) {
+function InputGroup({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="input-group"
@@ -19,19 +16,24 @@ function InputGroup({
       className={cn(
         "group/input-group border-input dark:bg-input/30 shadow-xs relative flex w-full items-center rounded-md border outline-none transition-[color,box-shadow]",
         "h-9 has-[>textarea]:h-auto",
+
         // Variants based on alignment.
         "has-[>[data-align=inline-start]]:[&>input]:pl-2",
         "has-[>[data-align=inline-end]]:[&>input]:pr-2",
         "has-[>[data-align=block-start]]:h-auto has-[>[data-align=block-start]]:flex-col has-[>[data-align=block-start]]:[&>input]:pb-3",
         "has-[>[data-align=block-end]]:h-auto has-[>[data-align=block-end]]:flex-col has-[>[data-align=block-end]]:[&>input]:pt-3",
+
         // Focus state.
         "has-[[data-slot=input-group-control]:focus-visible]:ring-ring has-[[data-slot=input-group-control]:focus-visible]:ring-1",
+
         // Error state.
         "has-[[data-slot][aria-invalid=true]]:ring-destructive/20 has-[[data-slot][aria-invalid=true]]:border-destructive dark:has-[[data-slot][aria-invalid=true]]:ring-destructive/40",
+
         className
       )}
-      {...props} />
-  );
+      {...props}
+    />
+  )
 }
 
 const inputGroupAddonVariants = cva(
@@ -59,7 +61,7 @@ function InputGroupAddon({
   className,
   align = "inline-start",
   ...props
-}) {
+}: React.ComponentProps<"div"> & VariantProps<typeof inputGroupAddonVariants>) {
   return (
     <div
       role="group"
@@ -67,29 +69,33 @@ function InputGroupAddon({
       data-align={align}
       className={cn(inputGroupAddonVariants({ align }), className)}
       onClick={(e) => {
-        if ((e.target).closest("button")) {
+        if ((e.target as HTMLElement).closest("button")) {
           return
         }
         e.currentTarget.parentElement?.querySelector("input")?.focus()
       }}
-      {...props} />
-  );
+      {...props}
+    />
+  )
 }
 
-const inputGroupButtonVariants = cva("flex items-center gap-2 text-sm shadow-none", {
-  variants: {
-    size: {
-      xs: "h-6 gap-1 rounded-[calc(var(--radius)-5px)] px-2 has-[>svg]:px-2 [&>svg:not([class*='size-'])]:size-3.5",
-      sm: "h-8 gap-1.5 rounded-md px-2.5 has-[>svg]:px-2.5",
-      "icon-xs":
-        "size-6 rounded-[calc(var(--radius)-5px)] p-0 has-[>svg]:p-0",
-      "icon-sm": "size-8 p-0 has-[>svg]:p-0",
+const inputGroupButtonVariants = cva(
+  "flex items-center gap-2 text-sm shadow-none",
+  {
+    variants: {
+      size: {
+        xs: "h-6 gap-1 rounded-[calc(var(--radius)-5px)] px-2 has-[>svg]:px-2 [&>svg:not([class*='size-'])]:size-3.5",
+        sm: "h-8 gap-1.5 rounded-md px-2.5 has-[>svg]:px-2.5",
+        "icon-xs":
+          "size-6 rounded-[calc(var(--radius)-5px)] p-0 has-[>svg]:p-0",
+        "icon-sm": "size-8 p-0 has-[>svg]:p-0",
+      },
     },
-  },
-  defaultVariants: {
-    size: "xs",
-  },
-})
+    defaultVariants: {
+      size: "xs",
+    },
+  }
+)
 
 function InputGroupButton({
   className,
@@ -97,35 +103,35 @@ function InputGroupButton({
   variant = "ghost",
   size = "xs",
   ...props
-}) {
+}: Omit<React.ComponentProps<typeof Button>, "size"> &
+  VariantProps<typeof inputGroupButtonVariants>) {
   return (
     <Button
       type={type}
       data-size={size}
       variant={variant}
       className={cn(inputGroupButtonVariants({ size }), className)}
-      {...props} />
-  );
+      {...props}
+    />
+  )
 }
 
-function InputGroupText({
-  className,
-  ...props
-}) {
+function InputGroupText({ className, ...props }: React.ComponentProps<"span">) {
   return (
     <span
       className={cn(
         "text-muted-foreground flex items-center gap-2 text-sm [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none",
         className
       )}
-      {...props} />
-  );
+      {...props}
+    />
+  )
 }
 
 function InputGroupInput({
   className,
   ...props
-}) {
+}: React.ComponentProps<"input">) {
   return (
     <Input
       data-slot="input-group-control"
@@ -133,14 +139,15 @@ function InputGroupInput({
         "flex-1 rounded-none border-0 bg-transparent shadow-none focus-visible:ring-0 dark:bg-transparent",
         className
       )}
-      {...props} />
-  );
+      {...props}
+    />
+  )
 }
 
 function InputGroupTextarea({
   className,
   ...props
-}) {
+}: React.ComponentProps<"textarea">) {
   return (
     <Textarea
       data-slot="input-group-control"
@@ -148,8 +155,9 @@ function InputGroupTextarea({
         "flex-1 resize-none rounded-none border-0 bg-transparent py-3 shadow-none focus-visible:ring-0 dark:bg-transparent",
         className
       )}
-      {...props} />
-  );
+      {...props}
+    />
+  )
 }
 
 export {
--- src/components/user/personalProfile/AvatarSection.tsx
+++ src/components/user/personalProfile/AvatarSection.tsx
@@ -1,28 +1,90 @@
+"use client"
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent } from '@/components/ui/card'
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
+import { Input } from '@/components/ui/input'
 import { getInitials } from '@/lib/formatter'
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import type { Blob } from 'buffer'
+import { toast } from 'sonner'
 
 export default function AvatarSection({ className }: { className?: string }) {
+    const [open, setOpen] = useState(false);
+    const ref = useRef<HTMLInputElement>(null);
+    const [imageSrc, setImageSrc] = useState<string | ArrayBuffer | null>(null);
+    const [crop, setCrop] = useState<{ x: number, y: number }>({ x: 0, y: 0 });
+    const [zoom, setZoom] = useState(1);
+    const onFileChange = () => {
+        const file = ref.current.files[0];
+        if (file.size > 15 * 1024 * 1024) {
+            toast.error("Kích thước ảnh vượt quá 15MB");
+            ref.current.value = null;
+            return;
+        }
+        if (!file.type.startsWith("image/")) {
+            toast.error("Vui lòng chọn tệp ảnh hợp lệ (PNG, JPEG, ...)");
+            ref.current.value = null;
+            return;
+        }
+        const reader = new FileReader();
+        if (file) {
+            reader.onload = () => {
+                setImageSrc(reader.result);
+                setOpen(true);
+            }
+            reader.readAsDataURL(file);
+        }
+    }
+
+    const saveAvatar = () => {
+
+    }
     return (
-        <Card className={`${className}`}>
-            <CardContent className='flex justify-between gap-4 items-center' >
-                <div className='flex gap-2 items-center mt-5'>
-                    <Avatar className="w-32 h-32">
-                        <AvatarImage src="/avatar.png" alt="User Avatar" />
-                        <AvatarFallback>{getInitials("Minh")}</AvatarFallback>
-                    </Avatar>
-                    <div className='flex flex-col gap-1' >
-                        <p>Ảnh đại diện</p>
-                        <p className='text-zinc-400 text-sm' >PNG, JPEG dưới 15MB</p>
+        <div>
+            <Card className={`${className}`}>
+                <CardContent className='flex justify-between gap-4 items-center'>
+                    <div className='flex gap-2 items-center mt-5'>
+                        <Avatar className="w-32 h-32">
+                            <AvatarImage src="/avatar.png" alt="User Avatar" />
+                            <AvatarFallback>{getInitials("Minh")}</AvatarFallback>
+                        </Avatar>
+                        <div className='flex flex-col gap-1' >
+                            <p>Ảnh đại diện</p>
+                            <p className='text-zinc-400 text-sm' >PNG, JPEG dưới 15MB</p>
+                        </div>
                     </div>
-                </div>
-                <div className='flex  gap-2 justify-center' >
-                    <Button className={'cursor-pointer'}>Đăng tải ảnh</Button>
-                    <Button variant="outline" className={'cursor-pointer'}>Xóa</Button>
-                </div>
-            </CardContent>
-        </Card>
+                    <div className='flex  gap-2 justify-center' >
+                        <Button className={'cursor-pointer'} onClick={() => ref.current.click()}>Đăng tải ảnh</Button>
+                        <Button variant="outline" className={'cursor-pointer'}>Xóa</Button>
+                    </div>
+                    <Input type='file' className='hidden' accept="image/*" ref={ref} onChange={onFileChange} />
+                </CardContent>
+            </Card>
+            <Dialog open={open} onOpenChange={(state) => {
+                setOpen(state);
+                if (!state) {
+                    setImageSrc(null);
+                    ref.current.value = null;
+                }
+            }} >
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Chọn ảnh đại diện</DialogTitle>
+                        <DialogDescription>Chỉnh sửa ảnh</DialogDescription>
+                    </DialogHeader>
+                    <div>
+                        <Avatar className="w-32 h-32 mx-auto mb-4">
+                            <AvatarImage src={imageSrc as string} alt="User Avatar" />
+                            <AvatarFallback>{getInitials("Minh")}</AvatarFallback>
+                        </Avatar>
+                    </div>
+                    <DialogFooter>
+                        <Button variant="outline" onClick={() => { setOpen(false); setImageSrc(null); ref.current.value = null; }}>Hủy</Button>
+                        <Button>Lưu</Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+        </div>
     )
 }
