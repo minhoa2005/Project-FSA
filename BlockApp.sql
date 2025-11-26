99b0dfb090eab68ae356584085f304d7a4c1b960
Tran Duc Minh <minhsoc5@gmail.com>
2025-11-26 14:21:58 +0700

    đă

--- BlockApp.sql
+++ BlockApp.sql
@@ -134,6 +134,15 @@ CREATE TABLE Messages (
 );
 GO
 
+CREATE TABLE Reports (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    blogId INT NULL,
+    reason VARCHAR(255) NOT NULL,
+    status VARCHAR(20) DEFAULT 'Pending',
+    createdAt DATETIME DEFAULT GETDATE(),
+    FOREIGN KEY (blogId) REFERENCES Blogs(id),
+)
+
 GO
 
 IF NOT EXISTS (SELECT 1 FROM Role WHERE roleName = 'Admin')
