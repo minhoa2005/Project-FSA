8ce58484e0fb17896586a20d3b79a64ed0e70a7d
hoangpzo <filmmusic249@gmail.com>
2025-11-27 10:59:56 +0700

    update changepass admin

--- src/components/admin/ChangePassAdmin.jsx
+++ src/components/admin/ChangePassAdmin.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from '../ui/sheet'
 import { Label } from '../ui/label'
 import { Input } from '../ui/input'
 import { Button } from '../ui/button'
+import { changePassAdmin, getAdminInfo } from '@/service/admin/admininfo'
+import bcrypt from 'bcryptjs'
+import { toast } from 'sonner'
 
 export default function ChangePassAdmin() {
+    const [err, setErr] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const data = new FormData(e.target)
+
+        const oldPass = data.get('oldPassword')
+        const newPass = data.get('newPassword')
+        const reNewPass = data.get('reNewPassword')
+
+        const infoAdmin = await getAdminInfo()
+        if(infoAdmin.success) {
+            const pass = infoAdmin?.data?.password
+            // console.log(pass)
+
+            const checkPass = await bcrypt.compare(oldPass, pass);
+            if (!checkPass) {
+                // toast.error('Mật khẩu cũ sai!')
+                setErr('Mật khẩu cũ sai!')
+                return;
+            }
+
+            if(newPass !== reNewPass) {
+                // toast.error('Mật khẩu mới không khớp!')
+                setErr('Mật khẩu mới không khớp!')
+                return;
+            }
+
+            const changePass = await changePassAdmin(newPass)
+                if(changePass) {
+                    setErr('Đổi mật khẩu thành công')
+                } else {
+                    setErr('Lỗi')
+                }
+        }
+
+    }
     return (
         <div>
             <Sheet>
@@ -13,27 +54,30 @@ export default function ChangePassAdmin() {
                 </SheetTrigger>
                 <SheetContent>
                     <SheetHeader>
-                        <SheetTitle>Đặt mật khẩu</SheetTitle>
+                        <SheetTitle className='text-lg'>Đặt mật khẩu</SheetTitle>
                         <SheetDescription>
-                            xxx
+                            Bạn cần xác nhận mật khẩu cũ trước khi cập nhật mật khẩu mới!
                         </SheetDescription>
                     </SheetHeader>
-                    <div className="grid flex-1 auto-rows-min gap-6 px-4">
-                        <div className="grid gap-3">
-                            <Label htmlFor="sheet-demo-name">Mật khẩu cũ</Label>
-                            <Input id="old-password" type='password' />
+                    <form id='resetPass' onSubmit={handleSubmit}>
+                        <div className="grid flex-1 auto-rows-min gap-6 px-4">
+                            <div className="grid gap-3">
+                                <Label htmlFor="old-password">Mật khẩu cũ</Label>
+                                <Input id="old-password" type='password' name='oldPassword' required />
+                            </div>
+                            <div className="grid gap-3">
+                                <Label htmlFor="new-password">Mật khẩu mới</Label>
+                                <Input id="new-password" type='password' name='newPassword' required />
+                            </div>
+                            <div className="grid gap-3">
+                                <Label htmlFor="re-new-password">Xác nhận mật khẩu mới</Label>
+                                <Input id="re-new-password" type='password' name='reNewPassword' required />
+                            </div>
+                            <div className='italic text-sm text-red-500'>{err}</div>
                         </div>
-                        <div className="grid gap-3">
-                            <Label htmlFor="sheet-demo-username">Mật khẩu mới</Label>
-                            <Input id="new-password" type='password' />
-                        </div>
-                        <div className="grid gap-3">
-                            <Label htmlFor="sheet-demo-username">Xác nhận mật khẩu mới</Label>
-                            <Input id="re-new-password" type='password' />
-                        </div>
-                    </div>
+                    </form>
                     <SheetFooter>
-                        <Button type="submit">Lưu</Button>
+                        <Button form='resetPass' type="submit">Lưu</Button>
                         <SheetClose asChild>
                             <Button variant="outline">Hủy</Button>
                         </SheetClose>
