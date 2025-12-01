d5f8430ac2252cf04443a0fea5b56aa1f29990b3
Tran Duc Minh <minhsoc5@gmail.com>
2025-12-01 09:04:34 +0700

    update personal page

--- src/components/user/personalPage/CoverSection.tsx
+++ src/components/user/personalPage/CoverSection.tsx
@@ -2,13 +2,13 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Card } from '@/components/ui/card'
 import { getInitials } from '@/lib/formatter'
 import Image from 'next/image'
-import React from 'react'
+import React, { use } from 'react'
 
 export default function CoverSection({ className, user }: { className?: string, user: any }) {
     return (
         <div className={`flex justify-center mt-2 ${className}`}>
             <div className='w-[70%] h-90 relative rounded' >
-                <Image src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29uYWwlMjBwYWdlfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60"
+                <Image src={user?.coverImg || ''}
                     alt="Personal Image"
                     layout="fill"
                     objectFit='cover'
