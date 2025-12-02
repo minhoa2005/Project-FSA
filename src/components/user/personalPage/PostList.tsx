8d12f25f0f1f3265adb55ed7c325834ace5d79e0
Tran Duc Minh <minhsoc5@gmail.com>
2025-12-02 13:28:37 +0700

    fix home page

--- src/components/user/personalPage/PostList.tsx
+++ src/components/user/personalPage/PostList.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { getPersonalBlogs } from '@/service/users/postActions'
 import React, { useEffect, useEffectEvent, useState } from 'react'
 import PostCard from './PostCard';
@@ -6,19 +6,22 @@ import CreatePost from './CreatePost';
 
 export default function PostList({ id }: { id: number }) {
     const [offset, setOffset] = useState(0);
-    // const posts = await getPersonalBlogs(id);
     const [posts, setPosts] = useState<any[]>([]);
     const fetchBlogs = useEffectEvent(async () => {
         const response = await getPersonalBlogs(id);
         setPosts(response.data);
     });
+    const refreshData = async () => {
+        const response = await getPersonalBlogs(id);
+        setPosts(response.data);
+    }
     useEffect(() => {
         fetchBlogs();
     }, []);
 
     return (
         <div className='flex flex-col gap-5 items-center'>
-            <CreatePost />
+            <CreatePost refresh={refreshData} />
             {posts.map((post, i) => (
                 <div key={post.id} className='w-[50%]' id='posts'>
                     <PostCard post={post} />
